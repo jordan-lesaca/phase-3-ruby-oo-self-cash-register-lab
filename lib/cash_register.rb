8e5dd41df840d4fe9e5639ac938eac1e2d19798e
Jordan Lesaca <jordan.m.lesaca@gmail.com>
2021-10-20 12:02:04 -0700

    commit tree cb7c237e92dc92d26c692e0f74a77a0a1dbca620

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,36 @@
+require 'pry'
+class CashRegister
+    attr_accessor :total , :discount , :cost
 
+    def initialize(discount=0)
+        @total = 0 
+        @discount = discount
+        @items = []
+    end
+
+    def add_item(title, price, quantity=1)
+        self.cost = price * quantity 
+        self.total += self.cost
+        quantity.times do
+            @items << title
+        end
+    end
+
+    def apply_discount
+        if discount != 0
+            @total = total - (total * discount.to_f/100)
+            "After the discount, the total comes to $#{@total.to_i}."
+        else 
+            "There is no discount to apply."
+        end
+    end
+
+    def items
+        @items
+    end
+
+    def void_last_transaction
+        @total = @total - @cost
+    end
+
+end
